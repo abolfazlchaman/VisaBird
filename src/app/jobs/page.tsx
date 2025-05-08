ba396d5bdc60a89d020d5934bc83cd0d29a19486
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-05-09 00:21:19 +0330

    added better title

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -26,12 +26,12 @@ export default async function JobsPage({ searchParams }: JobsPageProps) {
       {/* Main Content */}
       <main className="flex-grow">
         <div className="container mx-auto px-4 py-8 bg-slate-50 dark:bg-gray-900">
-          <div className="flex justify-between items-center mb-8">
-            <h1 className="text-3xl font-bold text-gray-800 dark:text-white">
-              Visa Sponsorship Jobs
+          <div className="flex justify-center items-center mb-8 text-center w-full">
+            <h1 className="text-3xl font-bold text-gray-800 dark:text-white text-center justify-self-center">
+              Curated List of Visa Sponsorship Companies
             </h1>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 xl:grid-cols-4 gap-8">
             {/* Filters Sidebar */}
             <div className="md:col-span-1">
               <JobSearch initialFilters={filters} />
