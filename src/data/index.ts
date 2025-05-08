01d5998fd20d4aefcba14c60a52617689a2a14c7
Abolfazl Chaman <abolfazlchaman.info@gmail.com>
2025-05-08 21:57:12 +0330

    added data for countries

---
+++ src/data/index.ts
@@ -0,0 +1,69 @@
+import { Company } from '@/types/jobs';
+import austria from './austria.json';
+import belgium from './belgium.json';
+import england from './england.json';
+import finland from './finland.json';
+import france from './france.json';
+import germany from './germany.json';
+import ireland from './ireland.json';
+import italy from './italy.json';
+import netherlands from './netherlands.json';
+import newZealand from './new-zealand.json';
+import norway from './norway.json';
+import spain from './spain.json';
+import sweden from './sweden.json';
+import turkey from './turkey.json';
+import denmark from './denmark.json';
+
+const transformCompany = (company: any): Company => ({
+  name: company.name,
+  yearFounded: company['year-founded'],
+  industry: company.industry,
+  city: company.city,
+  numberOfEmployees: company['number-of-employees'],
+  country: company.country,
+  linkedin: company.linkedin || '',
+});
+
+const transformCompanies = (companies: any[]): Company[] =>
+  companies.map(transformCompany);
+
+const countryDataAliases: Record<string, Company[]> = {
+  'Austria': transformCompanies(austria),
+  'United Kingdom': transformCompanies(england),
+  'Finland': transformCompanies(finland),
+  'France': transformCompanies(france),
+  'Germany': transformCompanies(germany),
+  'Ireland': transformCompanies(ireland),
+  'Italy': transformCompanies(italy),
+  'Netherlands': transformCompanies(netherlands),
+  'New Zealand': transformCompanies(newZealand),
+  'Norway': transformCompanies(norway),
+  'Denmark': transformCompanies(denmark),
+  'Belgium': transformCompanies(belgium),
+  'Turkey': transformCompanies(turkey),
+  'Spain': transformCompanies(spain),
+  'Sweden': transformCompanies(sweden),
+};
+
+export const ALL_COUNTRIES = (): Company[] => {
+  return [
+    ...transformCompanies(austria),
+    ...transformCompanies(belgium),
+    ...transformCompanies(denmark),
+    ...transformCompanies(england),
+    ...transformCompanies(finland),
+    ...transformCompanies(france),
+    ...transformCompanies(germany),
+    ...transformCompanies(ireland),
+    ...transformCompanies(italy),
+    ...transformCompanies(netherlands),
+    ...transformCompanies(newZealand),
+    ...transformCompanies(norway),
+    ...transformCompanies(spain),
+    ...transformCompanies(sweden),
+    ...transformCompanies(turkey),
+  ];
+};
+
+export { countryDataAliases }; 
